f386f9b2cebbc91d63b234299982e8fd198b6ae7
NennyMaryani <222310162@smkn1padaherang.sch.id>
2026-06-06 19:20:35 +0700

    menambahkan fungsi registre user

--- auth/auth.cpp
+++ auth/auth.cpp
@@ -33,5 +33,57 @@ void login() {
 }
 
 void registerUser() {
+    string username, password;
+    string id, user, pass, role;
+    int lastId = 0;
 
+    cout << "\n====== Register user ======\n";
+    cout << "Username : ";
+    cin >> username;
+
+    cout << "Password : ";
+    cin >> password;
+
+    // ambil ID terakhir
+    ifstream baca("auth/user.txt");
+
+    if(!baca.is_open()) {
+        cout << "File user.txt tidak ditemukan!\n";
+        return;
+    }
+
+    while(getline(baca, id, '|')) {
+        getline(baca, user, '|');
+        getline(baca, pass, '|');
+        getline(baca, role);
+
+        // cek username duplikat
+        if(username == user) {
+            cout << "\nUsername sudah digunakan!\n";
+            baca.close();
+            return;
+        }
+        lastId = stoi(id);
+    }
+
+    baca.close();
+
+    // simpan user baru
+    ofstream file("auth/user.txt", ios::app);
+
+    if(!file.is_open()) {
+        cout << "Gagal membuka file user.txt!\n";
+        return;
+    }
+
+    file << endl 
+         << lastId + 1
+         << "|" << username
+         << "|" << password
+         << "|user";
+
+    file.close();
+
+    cout << "\nRegistrasi berhasil!\n";
+    cout << "ID User : " << lastId + 1 << endl;
 }
